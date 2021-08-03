c175b0b5b120663f6d3f3f02ff157c17212cf0ac
Martin Sisk <Brothermj@hotmail.com>
2021-08-03 00:10:45 -0500

    fixed miss spelling variable error codes.

--- CollectionsAndStuff/Program.cs
+++ CollectionsAndStuff/Program.cs
@@ -7,14 +7,15 @@ namespace CollectionsAndStuff
     {
         static void Main(string[] args)
         {
-            // All of these methods that are on a List
+            // All of these methods mutate the original collection
 
 
-            // List<T> is a generic type, a type that requires me to tell it what type of stuff it does/Likes
+            // List<T> is a generic type, a type that requires me to tell it what type of stuff it does/uses
             // in this case 'string' is a type of parameter used to tell the List<T> how to work
             var e14Names = new List<string>();
 
             //add a single item
+            //works like push, appends to the end of the list
             e14Names.Add("Martin");
             e14Names.Add("Chie");
             e14Names.Add("Holly");
@@ -23,12 +24,15 @@ namespace CollectionsAndStuff
             e14Names.Insert(1, "Chris");
 
             // collection initializer - curly braces with elements inside
-            var teacherNames = new List<string> { "Nathan", "Jameka", "Dylan" };
+            var teacherNames = new List<string> { "Nathan", "Jameka", "Dylan", "Tom" };
 
             // add one collection to the other
             e14Names.AddRange(teacherNames);
 
             // remove tom (this uses a thing called reference equality)
+            e14Names.Remove("Tom");
+
+            // remove by index
             e14Names.RemoveAt(0);
 
             // remove by expressoion
@@ -41,19 +45,23 @@ namespace CollectionsAndStuff
             }
 
             // inline ForEach loop over a List
-            // list specific loop, takes in an action<T> (like a fat arrow fucntion)
+            // list specific loop, takes in an Action<T> (like a fat arrow fucntion)
             e14Names.ForEach(name => Console.WriteLine($"{name} is in e14 again!"));
 
+            // get the item at the index of 0  
             var firstStudent = e14Names[0];
 
             // Dictionary<Tkey, TValue> -- Open Generic (no one has specified what type of thing it is yet)
+            // Arity (`2) => how many generic type parameters a type has. Dictionary`2
             // like a physical dictionary, kinda
             // Keys have to be unique
             // our dictionary is keyed by strings, and has string values
+            // Good for: infrequently updated but often read data
+            // Good for: loading information at startup or in the background and fast retrieval on demand (caching)
             var dictionary = new Dictionary<string, string>(); // closed generic (we have told it how to behave)
 
             // adding things requires a key and a value
-            dictionary.Add("penultimate", "second to last");
+            dictionary.Add("Penultimate", "second to last");
             dictionary.Add("Jib", "The things that stick out of rollercoasters");
             dictionary.Add("Arbitrary", "Someone who doesn't like Arby's");
 
@@ -62,19 +70,19 @@ namespace CollectionsAndStuff
             // Console.WriteLine(definition);
 
             // this won't work : dictionaries require each key to be unique
-            // dictionary.Add("Penultimate", "Thing said too many times a the olympics");
+            // dictionary.Add("Penultimate", "Thing said too many times at the Olympics");
 
 
             // Try methods return a boolean indicating success or failure
             // kind of expensive just to find out if the key exists
-          if   (!dictionary.TryAdd("Penultimate", "Thing said too many times at the olympics"));
+          if   (!dictionary.TryAdd("Penultimate", "Thing said too many times at the Olympics"))
             {
                 Console.WriteLine("Its already in the dictionary, I couldn't add it");
             }
 
             if (dictionary.ContainsKey("Penultimate"))
             {
-                dictionary("Penultimate") = "Thing said too many times at the Olympics";
+                dictionary["Penultimate"] = "Thing said too many times at the Olympics";
             }
 
             // give me all the keys in a collection
@@ -92,7 +100,61 @@ namespace CollectionsAndStuff
                 Console.WriteLine($"{word}'s definition is {def}");
             }
 
+            var complicatedDictionary = new Dictionary<string, List<string>>();
 
+            complicatedDictionary.Add("Soup", new List<string> { "Hot or cold liquid you eat." });
+            var soupDefinitions = complicatedDictionary["Soup"];
+            soupDefinitions.Add("This is a definition of soup");
+
+            complicatedDictionary.Add("Arity", new List<string> { "A definition of arity" });
+
+            foreach (var (word, definitions) in complicatedDictionary)
+            {
+                Console.WriteLine(word);
+                foreach (var def in definitions)
+                {
+                    Console.WriteLine($"\t{def}");
+                }
+            }
+
+
+
+            // Hashset<T>
+            // Like a list in that they store a value at an index
+            // Like a dictionary in that the value has to be unique
+            // Completly different in that it eliminates non-unique stuff without errors
+            // pretty slow at adding data
+            // super fast getting data out, comparing data
+            //var jamekasName = "Jameka";
+            //jamekasName.GetHashCode();
+
+            var uniqueNames = new HashSet<string>();
+            uniqueNames.Add("jameka");
+            uniqueNames.Add("Dylan");
+
+            uniqueNames.Remove("Dylan");
+            
+            foreach(var name in uniqueNames)
+            {
+                Console.WriteLine($"{name} is unique");
+            }
+
+            //Queue<T>
+            //FIFO based collection
+
+            var queue = new Queue<int>();
+            queue.Enqueue(3);
+            queue.Enqueue(1);
+            queue.Enqueue(5);
+            queue.Enqueue(7);
+            queue.Enqueue(2);
+            queue.Enqueue(100);
+            queue.Enqueue(6);
+
+            while(queue.Count > 0)
+            {
+                Console.WriteLine($"dequeuing  {queue.Dequeue()}");
+            }
 
 
 
